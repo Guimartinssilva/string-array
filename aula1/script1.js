182dc1854cfb13bc5bf2052824f5ce5317bc4d17
Guimartinssilva <guimartinssilva23@gmail.com>
2024-10-03 21:55:56 -0300

    Exercícios de arrays

---
+++ aula1/script1.js
@@ -0,0 +1,6 @@
+let nome = prompt("Qual é o seu nome?")
+let cor = prompt("Qual é a sua cor favorita?")
+
+console.log(`A cor favorita de ${nome} é ${cor}`)
+
+console.log('A cor favorita de ' + nome + ' é ' + cor);
